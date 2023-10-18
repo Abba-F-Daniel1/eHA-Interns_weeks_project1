073d5a9ef4945a7b281a4a09a9a83747221df304
Mukhtar.Mahmuh <mukhtar.mahmuh@MukhtarMahmud.local>
2023-10-18 08:56:38 +0100

    configure tailwind file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useState } from "react";
+import React from "react";
 
 import "./App.css";
 
-function App() {
-
-  return (
-    <>
-      <div>
-        <h1>Hello, World</h1>
+const App: React.FC =()=>{
+  return(
+    <div className="bg-blue-400 h-screen flex items-center justify-center">
+      <div className="bg-white p-4 rounded-lg shadow-lg">
+         <h1 className="text-3xl text-center">Hello</h1>
       </div>
-    </>
-  );
+    </div>
+  )
 }
 
 export default App;
